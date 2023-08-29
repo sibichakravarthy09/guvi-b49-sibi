940253836b82f39a02e56c82b6f3339c53ca2835
sibi <arjunansibichakravarthy007@gmail.com>
2023-08-29 15:26:48 +0530

    Day-14

--- Day-14.3/index.js
+++ Day-14.3/index.js
@@ -2,22 +2,22 @@ const form = document.getElementById("myform");
 const table = document.getElementById("dataTable");
 
 form.addEventListener("submit", function (event) {
-  event.preventDefault();
-// Get the values from the form
+ event.preventDefault();
+
 const firstName = document.getElementById("firstName").value;
 const lastName = document.getElementById("lastName").value;
 const gender = document.querySelector('input[name="gender"]:checked').value;
 const food = [];
 document.querySelectorAll('input[name="food"]:checked').forEach((checkbox) => {
-  food.push(checkbox.value);
+ food.push(checkbox.value);
 });
 const address = document.getElementById("address").value;
 const state = document.getElementById("state").value;
 const country = document.getElementById("country").value;
 const pincode = document.getElementById("pincode").value;
-// Create a new row in the table
+
 const row = document.createElement("tr");
-// Create cells for each column
+
 const nameCell = document.createElement("td");
 const genderCell = document.createElement("td");
 const foodCell = document.createElement("td");
@@ -25,7 +25,7 @@ const addressCell = document.createElement("td");
 const stateCell = document.createElement("td");
 const countryCell = document.createElement("td");
 const pincodeCell = document.createElement("td");
-// Add the values to the cells
+
 nameCell.innerHTML = `${firstName} ${lastName}`;
 genderCell.innerHTML = gender;
 foodCell.innerHTML = food.join(", ");
@@ -33,7 +33,7 @@ addressCell.innerHTML = address;
 stateCell.innerHTML = state;
 countryCell.innerHTML = country;
 pincodeCell.innerHTML = pincode;
-// Add the cells to the row
+
 row.appendChild(nameCell);
 row.appendChild(genderCell);
 row.appendChild(foodCell);
@@ -41,7 +41,7 @@ row.appendChild(addressCell);
 row.appendChild(stateCell);
 row.appendChild(countryCell);
 row.appendChild(pincodeCell);
-// Add the row to the table
+
 const table = document.getElementById("dataTable");
 table.appendChild(row);
 form.reset();
