aa1343328376b8dbe666ff0b7a28ecd73506808a
sibi <arjunansibichakravarthy007@gmail.com>
2023-08-31 10:38:10 +0530

    Day-16

---
+++ Day-16.2/script.js
@@ -0,0 +1,57 @@
+
+const myDiv = document.getElementById("myDiv");
+const myH1 = document.getElementById("myH1");
+
+
+setTimeout(function() {
+    myH1.innerHTML="10";
+    
+    setTimeout(function() {
+        myH1.innerHTML="9";
+        
+        setTimeout(function() {
+            myH1.innerHTML="8";
+            
+            setTimeout(function() {
+                myH1.innerHTML="7";
+                
+                setTimeout(function() {
+                    myH1.innerHTML="6";
+                    
+                    setTimeout(function() {
+                        myH1.innerHTML="5";
+                        
+                        setTimeout(function() {
+                            myH1.innerHTML="4";
+                            
+                            setTimeout(function() {
+                                myH1.innerHTML="3";
+                                
+                                setTimeout(function() {
+                                    myH1.innerHTML="2";
+                                    
+                                    setTimeout(function() {
+                                        myH1.innerHTML="1";
+                                        
+                                        myH1.innerHTML="Happy Independence Day";
+                                    }, 1000);
+                                    
+                                }, 1000);
+                                
+                            }, 1000);
+                            
+                        }, 1000);
+                        
+                    }, 1000);
+                    
+                }, 1000);
+                
+            }, 1000);
+            
+        }, 1000);
+        
+    }, 1000);
+    
+}, 1000);
+
+
